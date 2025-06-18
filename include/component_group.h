bee4c507fcc57540aa5f360cd42bdae9d49f324d
gkennedy <graeme.kennedy@ae.gatech.edu>
2025-06-17 22:27:38 -0400

    modified coloring algorithm

--- include/component_group.h
+++ include/component_group.h
@@ -114,80 +114,180 @@ class OmpGroupBackend {
     layout.get_data(&length, &ncomps, &array);
     color_elements(length, array);
   }
-  int max_color;
-  std::vector<int> elem_ptr;
-  std::vector<int> elem_by_color;
+  ~OmpGroupBackend() {
+    delete[] elem_by_color_ptr;
+    delete[] elem_by_color;
+  }
+
+  void build_element_to_element(int nelems, const int *elem_nodes,
+                                int **elem_to_elem_ptr_, int **elem_to_elem_) {
+    int max_node = 0;
+    for (int i = 0; i < nelems * ncomp; i++) {
+      if (elem_nodes[i] > max_node) {
+        max_node = elem_nodes[i];
+      }
+    }
+    max_node++;
+
+    // Create a pointer from the nodes back to the elements
+    int *node_to_elem_ptr = new int[max_node + 1];
+    std::fill(node_to_elem_ptr, node_to_elem_ptr + max_node + 1, 0);
+
+    for (int i = 0; i < nelems * ncomp; i++) {
+      node_to_elem_ptr[elem_nodes[i] + 1]++;
+    }
+    for (int i = 0; i < max_node; i++) {
+      node_to_elem_ptr[i + 1] += node_to_elem_ptr[i];
+    }
+
+    int *node_to_elem = new int[nelems * ncomp];
+
+    // Fill in the element numbers
+    for (int i = 0; i < nelems; i++) {
+      for (int j = 0; j < ncomp; j++) {
+        int node = elem_nodes[ncomp * i + j];
+        node_to_elem[node_to_elem_ptr[node]] = i;
+        node_to_elem_ptr[node]++;
+      }
+    }
+
+    // Fix the now broken node_to_elem_ptr array
+    for (int i = max_node - 1; i >= 0; i--) {
+      node_to_elem_ptr[i + 1] = node_to_elem_ptr[i];
+    }
+    node_to_elem_ptr[0] = 0;
+
+    // Compute the element -> element data structure
+    int *elem_flags = new int[nelems];
+    std::fill(elem_flags, elem_flags + nelems, -1);
+
+    int *elem_to_elem_ptr = new int[nelems + 1];
+    for (int i = 0; i < nelems; i++) {
+      int count = 0;
+
+      for (int j = 0; j < ncomp; j++) {
+        int node = elem_nodes[ncomp * i + j];
+
+        // Find the adjacent elements
+        int start = node_to_elem_ptr[node];
+        int end = node_to_elem_ptr[node + 1];
+        for (int k = start; k < end; k++) {
+          int e = node_to_elem[k];
+
+          if (e != i && elem_flags[e] != i) {
+            count++;
+            elem_flags[e] = i;
+          }
+        }
+      }
+      elem_to_elem_ptr[e + 1] = count;
+    }
+
+    for (int i = 0; i < nelems; i++) {
+      elem_to_elem_ptr[i + 1] += elem_to_elem_ptr[i];
+    }
+
+    std::fill(elem_flags, elem_flags + nelems, -1);
+    int *elem_to_elem = new int[elem_to_elem_ptr[nelems]];
+    for (int i = 0; i < nelems; i++) {
+      int count = 0;
+
+      for (int j = 0; j < ncomp; j++) {
+        int node = elem_nodes[ncomp * i + j];
+
+        // Find the adjacent elements
+        int start = node_to_elem_ptr[node];
+        int end = node_to_elem_ptr[node + 1];
+        for (int k = start; k < end; k++) {
+          int e = node_to_elem[k];
+
+          if (e != i && elem_flags[e] != i) {
+            elem_to_elem[elem_to_elem_ptr[i] + count] = e;
+            elem_flags[e] = i;
+          }
+        }
+      }
+    }
+
+    delete[] node_to_elem;
+    delete[] node_to_elem_ptr;
+    delete[] elem_flags;
+
+    *elem_to_elem_ptr_ = elem_to_elem_ptr;
+    *elem_to_elem_ = elem_to_elem;
+  }
 
   // Input:
   // elem_nodes[e] = list of global node ids for element e
   // Output:
   // elem_color[e] = color assigned to element e
   void color_elements(int nelems, const int *elem_nodes) {
-    std::unordered_map<int, std::vector<int>> node_to_elems;
+    int *elem_to_elem_ptr, *elem_to_elem;
+    build_element_to_element(nelems, elem_nodes, &elem_to_elem_ptr,
+                             &elem_to_elem);
 
-    // Step 1: Build node to element mapping
-    for (int e = 0; e < nelems; ++e) {
-      for (int j = 0; j < ncomp; j++) {
-        int node = elem_nodes[ncomp * e + j];
-        node_to_elems[node].push_back(e);
-      }
-    }
+    // Greedy coloring
+    int *elem_colors = new int[nelems];
+    std::fill(elem_colors, elem_colors + nelems, -1);
 
-    // Step 2: Build element adjacency list
-    std::vector<std::unordered_set<int>> elem_neighbors(nelems);
-    for (const auto &[node, elems] : node_to_elems) {
-      for (size_t i = 0; i < elems.size(); ++i) {
-        for (size_t j = 0; j < elems.size(); ++j) {
-          if (i != j) {
-            elem_neighbors[elems[i]].insert(elems[j]);
-          }
-        }
-      }
-    }
+    // Keep track of the number of colors
+    num_colors = 0;
 
-    // Step 3: Greedy coloring
-    std::vector<int> elem_color(nelems, -1);
-    max_color = 0;
+    int *flags = new int[nelems];
+    std::fill(flags, flags + nelems, -1);
 
-    for (int e = 0; e < nelems; ++e) {
-      std::unordered_set<int> neighbor_colors;
-      for (int neigh : elem_neighbors[e]) {
-        int c = elem_color[neigh];
-        if (c != -1) {
-          neighbor_colors.insert(c);
+    for (int e = 0; e < nelems; e++) {
+      int start = elem_to_elem_ptr[e];
+      int end = elem_to_elem_ptr[e + 1];
+      for (int k = start; k < end; k++) {
+        int neigh = elem_to_elem[k];
+        int c = elem_colors[neigh];
+
+        if (c >= 0) {
+          flags[c] = e;
         }
       }
 
-      // Find smallest non-conflicting color
-      int color = 0;
-      while (neighbor_colors.count(color)) {
-        ++color;
+      // Find smallest non-conflicting color if any
+      int found = false;
+      for (int k = 0; k < num_colors; k++) {
+        if (flags[k] != e) {
+          elem_colors[e] = k;
+          found = true;
+          break;
+        }
       }
 
-      elem_color[e] = color;
-      max_color = std::max(max_color, color);
+      // No color was found, create a new color
+      if (!found) {
+        elem_colors[e] = num_colors;
+        num_colors++;
+      }
     }
 
     // Step 4: Set up the elements by color
-    elem_ptr.resize(max_color + 1, 0);
+    elem_by_color_ptr = new int[num_colors + 1];
     for (int e = 0; e < nelems; e++) {
-      elem_ptr[elem_color[e] + 1]++;
+      elem_by_color_ptr[elem_colors[e] + 1]++;
     }
 
-    for (int i = 0; i < max_color; i++) {
-      elem_ptr[i + 1] += elem_ptr[i];
+    for (int i = 0; i < num_colors; i++) {
+      elem_by_color_ptr[i + 1] += elem_by_color_ptr[i];
     }
 
     for (int e = 0; e < nelems; e++) {
-      elem_by_color[ptr[elem_color[e]]] = e;
-      ptr[elem_color[e]]++;
+      elem_by_color[elem_by_color_ptr[elem_colors[e]]] = e;
+      elem_by_color_ptr[elem_color[e]]++;
     }
 
     // Reset the pointer
-    for (int i = max_color - 1; i >= 0; i--) {
-      elem_ptr[i + 1] = elem_ptr[i];
+    for (int i = num_colors - 1; i >= 0; i--) {
+      elem_by_color_ptr[i + 1] = elem_by_color_ptr[i];
     }
-    elem_ptr[0] = 0;
+    elem_by_color_ptr[0] = 0;
+
+    delete[] elem_colors;
+    delete[] flags;
   }
 
   T lagrangian_kernel(const IndexLayout<ndata> &data_layout,
@@ -214,9 +314,9 @@ class OmpGroupBackend {
     Data data;
     Input input, gradient;
 
-    for (int j = 0; j < max_color; j++) {
-      int start = elem_ptr[j];
-      int end = elem_ptr[j + 1];
+    for (int j = 0; j < num_colors; j++) {
+      int start = elem_by_color_ptr[j];
+      int end = elem_by_color_ptr[j + 1];
 #pragma omp parallel for
       for (int i = start; i < end; i++) {
         int elem = elem_by_color[i];
@@ -237,9 +337,9 @@ class OmpGroupBackend {
     Data data;
     Input input, gradient, direction, result;
 
-    for (int j = 0; j < max_color; j++) {
-      int start = elem_ptr[j];
-      int end = elem_ptr[j + 1];
+    for (int j = 0; j < num_colors; j++) {
+      int start = elem_by_color_ptr[j];
+      int end = elem_by_color_ptr[j + 1];
 #pragma omp parallel for
       for (int i = start; i < end; i++) {
         int elem = elem_by_color[i];
@@ -259,11 +359,11 @@ class OmpGroupBackend {
                           const Vector<T> &data_vec, const Vector<T> &vec,
                           CSRMat<T> &jac) const {
     Data data;
-    Input input, gradient, direction, result[ncomp];
+    Input input, gradient, direction, result;
 
-    for (int j = 0; j < max_color; j++) {
-      int start = elem_ptr[j];
-      int end = elem_ptr[j + 1];
+    for (int j = 0; j < num_colors; j++) {
+      int start = elem_by_color_ptr[j];
+      int end = elem_by_color_ptr[j + 1];
 #pragma omp parallel for
       for (int i = start; i < end; i++) {
         int elem = elem_by_color[i];
@@ -272,30 +372,32 @@ class OmpGroupBackend {
         layout.get_indices(elem, index);
         layout.get_values(elem, vec, input);
 
-        for (int j = 0; j < Component::ncomp; j++) {
+        for (int k = 0; k < Component::ncomp; k++) {
           direction.zero();
           gradient.zero();
-          result[j].zero();
+          result.zero();
 
-          direction[j] = 1.0;
+          direction[k] = 1.0;
 
-          Component::hessian(data, input, direction, gradient, result[j]);
-        }
+          Component::hessian(data, input, direction, gradient, result);
 
-        for (int j = 0; j < Component::ncomp; j++) {
-          jac.add_row(index[j], Component::ncomp, index, result[j]);
+          jac.add_row(index[k], Component::ncomp, index, result);
         }
       }
     }
   }
+
+ private:
+  int num_colors;
+  int *elem_by_color_ptr;
+  int *elem_by_color;
 };
 
 template <typename T, class Component>
 using DefaultGroupBackend = OmpGroupBackend<T, Component>;
 #elif defined(AMIGO_USE_CUDA)
 
-#else
-
+#else  // Default to serial implementation
 template <typename T, class Component>
 using DefaultGroupBackend = SerialGroupBackend<T, Component>;
 #endif
