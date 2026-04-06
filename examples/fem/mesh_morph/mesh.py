b280e70ecfbf5ef50983041183f58baeb9b9e9e9
Seiyon Arulampalam <sarulampalam3@gatech.edu>
2026-04-06 18:31:51 -0400

    renamed model.py -> mesh_morph.py; mesh.py updated with argparse; deleted extra comments from mesh.py.

--- examples/fem/mesh_morph/mesh.py
+++ examples/fem/mesh_morph/mesh.py
@@ -1,12 +1,13 @@
 import gmsh
 import sys
+import argparse
 
 gmsh.initialize()
 gmsh.model.add("mesh")
 
 # Mesh refinement at nodes
-lc = 10e-1
-lc1 = 1e-1
+lc = 5e-1
+lc1 = 5e-1
 
 # Geometry dimentions
 hb = 5  # Boundary length and width
@@ -14,14 +15,14 @@ hm = 1  # Magnet length and width
 
 # Add points
 geom = gmsh.model.geo
-geom.addPoint(hb, -hb, 0, lc1, 1)
-geom.addPoint(hb, hb, 0, lc1, 2)
+geom.addPoint(hb, -hb, 0, lc, 1)
+geom.addPoint(hb, hb, 0, lc, 2)
 geom.addPoint(-hb, hb, 0, lc, 3)
 geom.addPoint(-hb, -hb, 0, lc, 4)
-geom.addPoint(hm, -hm, 0, lc, 5)
-geom.addPoint(hm, hm, 0, lc, 6)
-geom.addPoint(-hm, hm, 0, lc, 7)
-geom.addPoint(-hm, -hm, 0, lc, 8)
+geom.addPoint(hm, -hm, 0, lc1, 5)
+geom.addPoint(hm, hm, 0, lc1, 6)
+geom.addPoint(-hm, hm, 0, lc1, 7)
+geom.addPoint(-hm, -hm, 0, lc1, 8)
 
 # Define lines for the boundary loop
 geom.addLine(1, 2, 1)
@@ -47,16 +48,18 @@ geom.addPlaneSurface([2], 2)
 gmsh.model.geo.synchronize()
 
 # Generate 2d mesh
-# order = 1
-# gmsh.model.mesh.setRecombine(2, 1)
-# gmsh.model.mesh.setRecombine(2, 2)
 gmsh.model.mesh.generate(2)
-# gmsh.model.mesh.setOrder(order)  # set the order
 
 # Save the mesh
 gmsh.write(f"mesh.inp")
 
-if "-nopopup" not in sys.argv:
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--nopopup", action="store_true", default=False, help="Turn off GMSH popup"
+)
+args = parser.parse_args()
+
+if args.nopopup == False:
     gmsh.fltk.run()
 
 gmsh.finalize()
